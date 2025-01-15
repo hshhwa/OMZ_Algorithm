12fe382110e93231b65e122eda34572ac4a4cfe3
wjdn154 <gj1563@naver.com>
2025-01-15 21:08:40 +0900

    저장

--- src/KeonHo/P10808.java
+++ src/KeonHo/P10808.java
@@ -4,11 +4,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class P10808 {
+class P10808 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int[] arr = new int[26];
 
+        String s = br.readLine();
 
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            arr[c - 97]++;
+        }
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 }
