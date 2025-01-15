1a4d9bb86e0f0399408ab8a6135977d1037a6599
hshhwa <hshdla@naver.com>
2025-01-15 20:15:03 +0900

    테스트

--- src/HongHongs/P101.java
+++ src/HongHongs/P101.java
@@ -2,6 +2,6 @@ package HongHongs;
 
 public class P101 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("연습문제");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-package HongHongs;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
