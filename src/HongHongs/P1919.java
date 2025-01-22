6bb2a294202bfc20f5cfc481fab4c620f473b693
hshhwa <hshdla@naver.com>
2025-01-22 16:00:34 +0900

    저장

--- src/HongHongs/P1919.java
+++ src/HongHongs/P1919.java
@@ -5,36 +5,28 @@ import java.util.Scanner;
 public class P1919 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
-        String OneWord = scanner.nextLine();
-        String TwoWord = scanner.nextLine();
-
-        String OneWordTemp = OneWord;
-        String TwoWordTemp = TwoWord;
         int answer = 0;
 
-        for(int i = 0; i < OneWordTemp.length(); i++){
-            int index = TwoWord.indexOf(OneWordTemp.charAt(i));
-            if(index == -1){
-                OneWordTemp = OneWordTemp.substring(0, i) + "?" + OneWordTemp.substring(i + 1);
-                answer++;
-            }
-            else {
-                TwoWordTemp = TwoWordTemp.substring(0, index) + "?" + TwoWordTemp.substring(index + 1);
-            }
+        String oneWord = scanner.nextLine();
+        String twoWord = scanner.nextLine();
+
+        int[] oneAlphabet = new int[26];
+        int[] twoAlphabet = new int[26];
+
+
+        for (char c : oneWord.toCharArray()) {
+            oneAlphabet[c - 'a']++;
         }
 
-        for(int i = 0; i < TwoWordTemp.length(); i++){
-            int index = OneWord.indexOf(TwoWordTemp.charAt(i));
-            if(index == -1){
-                TwoWordTemp = TwoWordTemp.substring(0, i) + "?" + TwoWordTemp.substring(i + 1);
-                answer++;
-            }
-            else {
-                OneWordTemp = OneWordTemp.substring(0, index) + "?" + OneWordTemp.substring(index + 1);
-            }
+
+        for (char c : twoWord.toCharArray()) {
+            twoAlphabet[c - 'a']++;
         }
-        System.out.print(answer);
+
+        for (int i = 0; i < 26; i++) {
+            answer += Math.abs(oneAlphabet[i] - twoAlphabet[i]);
+        }
+
+        System.out.println(answer);
     }
 }
