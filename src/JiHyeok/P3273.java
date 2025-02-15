42cff83205202cd8bf0850b49f7cad74e62a38b1
jh <chlwlgur0407@naver.com>
2025-02-15 17:07:37 +0900

    add P3273

---
+++ src/JiHyeok/P3273.java
@@ -0,0 +1,42 @@
+package JiHyeok;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P3273 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int count = 0;
+        int x = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int left = 0, right = n - 1;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == x) {
+                count++;
+                left++;
+                right--;
+            } else if (sum < x) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        System.out.println(count);
+    }
+}
